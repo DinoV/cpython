a6bb723681ba2c62e198c33a1457c23adf7df712
Jacob Bower <jbower@meta.com>
2025-09-26 12:51:24 -0700

    Add patch 'remove-gentype-from-test'
    Summary:
    This diff adds a patch to the `3.14` Meta-internal fork.
    
    Removes explicit reference to PyGen_Type in a test so CinerX's JIT generators will also work
    
    Reviewed By: itamaro
    
    Differential Revision: D83307766
    
    fbshipit-source-id: 6f36988ab9df34ba8c2718087f58f6b422fa9e37

--- Modules/_testcapimodule.c
+++ Modules/_testcapimodule.c
@@ -1584,9 +1584,9 @@ getitem_with_error(PyObject *self, PyObject *args)
 static PyObject *
 raise_SIGINT_then_send_None(PyObject *self, PyObject *args)
 {
-    PyGenObject *gen;
+    PyObject *gen;
 
-    if (!PyArg_ParseTuple(args, "O!", &PyGen_Type, &gen))
+    if (!PyArg_ParseTuple(args, "O", &gen))
         return NULL;
 
     /* This is used in a test to check what happens if a signal arrives just
